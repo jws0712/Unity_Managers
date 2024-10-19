367a5acec2ced634d37064209b19bf1db3da8948
jws2170 <mpart2077@gmail.com>
2024-10-20 00:58:55 +0900

    ADD | ObjectPooling

---
+++ Assets/01.Scripts/ObjectPool/UnityObjectPooling/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool; //유니티 오브젝트 풀링 사용 using
+
+public class Bullet : MonoBehaviour
+{
+    private Vector3 _Direction;
+
+    [SerializeField]
+    private float _Speed = 3f;
+
+    //풀사용 코드
+    private IObjectPool<Bullet> _ManagedPool;
+
+    public void Shoot(Vector3 dir)
+    {
+        _Direction = dir;
+        // 5초가 지난 후 총알을 파괴
+        //Destroy(gameObject, 5f);
+
+        Invoke("DestroyObject", 5f);
+    }
+
+    //오브젝트를 풀안에 넣어줌
+    public void SetManagedPool(IObjectPool<Bullet> pool)
+    {
+        _ManagedPool = pool;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(_Direction * Time.deltaTime * _Speed);
+    }
+
+    //오브젝트를 릴리즈 해줌 Destory함수 대용
+    public void DestroyObject()
+    {
+        _ManagedPool.Release(this);
+    }
+}
---
+++ Assets/01.Scripts/ObjectPool/UnityObjectPooling/Shooter.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json.Bson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _BulletPrefab;
+
+    private Camera _MainCam;
+
+    private IObjectPool<Bullet> _Pool;
+
+    private void Awake()
+    {
+        _Pool = new ObjectPool<Bullet>(CreatBullet, OnGetBullet, OnReleaseBullet, OnDestroyBullet, maxSize:20);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _MainCam = Camera.main;
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit hitResult;
+            if(Physics.Raycast(_MainCam.ScreenPointToRay(Input.mousePosition), out hitResult))
+            {
+                var direction = new Vector3(hitResult.point.x, transform.position.y, hitResult.point.z) - transform.position;
+                // 총알을 새로 생성해서 사용
+                //var bullet = Instantiate(_BulletPrefab, transform.position + direction.normalized, Quaternion.identity).GetComponent<Bullet>();
+                var bullet = _Pool.Get();
+                bullet.transform.position = transform.position + direction.normalized;
+                bullet.Shoot(direction.normalized);
+            }
+        }
+    }
+
+    private Bullet CreatBullet()
+    {
+        Bullet bullet = Instantiate(_BulletPrefab).GetComponent<Bullet>();
+        bullet.SetManagedPool(_Pool);
+        return bullet;
+    }
+
+    private void OnGetBullet(Bullet bullet)
+    {
+        bullet.gameObject.SetActive(true);
+    }
+
+    private void OnReleaseBullet(Bullet bullet)
+    {
+        bullet.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyBullet(Bullet bullet)
+    {
+        Destroy(bullet.gameObject);
+    }
+}
